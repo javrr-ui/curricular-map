f61931b9f27cc358b825126effa24085662fa635
Javier <framirez76@uabc.edu.mx>
2021-08-20 23:13:16 -0700

    Show form data using alert

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("form")
+const label = document.querySelector("label");
+
+
+form.addEventListener("submit",showData)
+
+function showData(e){
+    alert(form)
+    
+}

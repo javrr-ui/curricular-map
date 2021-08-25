e573b7ed60ce04c5ae13e0e47f4c04b0af2f8426
Javier <framirez76@uabc.edu.mx>
2021-08-24 23:46:16 -0700

    Create subject object

--- js/subject.js
+++ js/subject.js
@@ -3,7 +3,17 @@ const subject = document.querySelectorAll(".subject");
 
 subject.forEach(item => {
     item.addEventListener("click", event => {
-        
+
+        const subject = { 
+            subjectName: item.querySelector(".subject-name").textContent,
+            classHours: item.querySelector(".top-left-square").textContent,
+            workshopHours: item.querySelector(".workshopHours").textContent,
+            labHours: item.querySelector(".top-right-square").textContent,
+            extraClassHours: item.querySelector(".bottom-left-square").textContent,
+            subjectId: item.querySelector(".subject-number").textContent,
+            credits: item.querySelector(".bottom-right-square").textContent,
+        }
+        console.log("materia: "+subject.subjectName)
         try {
             alert(item.querySelector(".subject-name").textContent);
         }catch(error){

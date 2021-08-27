991214cc4219d231f23633b284b96d974154a020
Javier <framirez76@uabc.edu.mx>
2021-08-26 23:10:28 -0700

    Add modal to show subject info when subject div is clicked

--- js/subject.js
+++ js/subject.js
@@ -1,35 +1,40 @@
 const subject = document.querySelectorAll(".subject");
+const subjectModal = document.querySelector(".subject-modal-background");
 
+//modal p's
+const subjectName = document.querySelector("#subjectName");
+const classHours = document.querySelector("#classHours");
+const workshopHours = document.querySelector("#workshopHours");
+const labHours = document.querySelector("#labHours");
+const extraClassHours = document.querySelector("#extraClassHours");
+const subjectId = document.querySelector("#subjectId");
+const credits = document.querySelector("#credits");
+
+subjectModal.addEventListener("click", e => {
+
+    subjectModal.classList.add("hideModal");
+    setTimeout(() => {
+        subjectModal.style.display = "none";
+    }, 200);
+
+})
 
 subject.forEach(item => {
     item.addEventListener("click", event => {
 
-        try {
-            const subject = {
-                subjectName: item.querySelector(".subject-name").textContent,
-                classHours: item.querySelector(".top-left-square").textContent,
-                workshopHours: item.querySelector(".workshopHours").textContent,
-                labHours: item.querySelector(".top-right-square").textContent,
-                extraClassHours: item.querySelector(".bottom-left-square").textContent,
-                subjectId: item.querySelector(".subject-number").textContent,
-                credits: item.querySelector(".bottom-right-square").textContent,
-            }
-            const subjectKeys = ["Materia: ", "Horas de clase: ", "Horas de taller: ",
-                "Horas de laboratorio: ", "Horas extra de clase: ", "Clave de materia: ", "Creditos: "];
-            const subjectInfo = [];
-            var i = 0;
-            Object.values(subject).forEach(val => {
-
-                if (val === null || val === undefined || val == "") {
-                    subjectInfo.push(subjectKeys[i] + "0" + "\n")
-                } else {
-                    subjectInfo.push(subjectKeys[i] + val + "\n")
-                }
-                i++;
-            })
-            alert(subjectInfo.toString().replace(/,/g, ""));
-        } catch (error) {
-            alert("Something weird happend...\n" + error)
-        }
+        subjectName.innerHTML = "Materia: " + item.querySelector(".subject-name").textContent;
+        classHours.innerHTML = "Horas de clase: " + item.querySelector(".top-left-square").textContent;
+        workshopHours.innerHTML = "Horas de taller: " + item.querySelector(".workshopHours").textContent;
+        labHours.innerHTML = "Horas de laboratorio: " + item.querySelector(".top-right-square").textContent;
+        extraClassHours.innerHTML = "Horas extra de clase: " + item.querySelector(".bottom-left-square").textContent;
+        subjectId.innerHTML = "Clave: " + item.querySelector(".subject-number").textContent;
+        credits.innerHTML = "Creditos: " + item.querySelector(".bottom-right-square").textContent;
+        showModal();
     })
 })
+
+function showModal() {
+
+    subjectModal.style.display = "block";
+    subjectModal.classList.remove("hideModal");
+}
